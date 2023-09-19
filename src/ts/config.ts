f208c9b1660f999e265d1ed71bb928e02023fbb8
Sophia Foxy Coxy <foxycoxy@thefoxesare.online>
2023-09-19 23:09:52 +1000

    Changed colors and added meme

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -20,20 +20,20 @@ const config: {
         ratio: 4,
         padding: 2,
         separator: {
-            light: '#FFFFFF80',
-            dark: '#00000080',
+            light: '#00000030',
+            dark: '#00000060',
         },
         background: {
-            mode: 'rgb' as InterpolationMode,
+            mode: 'rgb',
             light: {
                 color: '#FFFFFF',
-                ratio: 0.6
+                ratio: 0.75
             },
             dark: {
                 color: '#000000',
-                ratio: 0.6
+                ratio: 0.75
             },
-            alpha: 0.6
+            alpha: 0.75
         }
     }
 }
